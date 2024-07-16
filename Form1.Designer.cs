0c3ce19d05a2fdd1a16bb1da4c4c80cae34ae3ba
Carl Damke <carl.simon.damke@gmail.com>
2024-07-16 07:06:21 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace cod4x_fixer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private TextBox textBoxCodKey;
+        private Label labelCodKey;
+        private ComboBox comboBoxValues;
+        private Label labelAvKey;
+        private Button buttonUpdate;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.textBoxCodKey = new System.Windows.Forms.TextBox();
+            this.labelCodKey = new System.Windows.Forms.Label();
+            this.comboBoxValues = new System.Windows.Forms.ComboBox();
+            this.labelAvKey = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxCodKey
+            // 
+            this.textBoxCodKey.Location = new System.Drawing.Point(12, 29);
+            this.textBoxCodKey.Name = "textBoxCodKey";
+            this.textBoxCodKey.ReadOnly = true;
+            this.textBoxCodKey.Size = new System.Drawing.Size(400, 20);
+            this.textBoxCodKey.TabIndex = 0;
+            // 
+            // labelCodKey
+            // 
+            this.labelCodKey.AutoSize = true;
+            this.labelCodKey.Location = new System.Drawing.Point(12, 9);
+            this.labelCodKey.Name = "labelCodKey";
+            this.labelCodKey.Size = new System.Drawing.Size(48, 13);
+            this.labelCodKey.TabIndex = 1;
+            this.labelCodKey.Text = "Aktueller Key";
+            // 
+            // comboBoxValues
+            // 
+            this.comboBoxValues.FormattingEnabled = true;
+            this.comboBoxValues.Location = new System.Drawing.Point(12, 84);
+            this.comboBoxValues.Name = "comboBoxValues";
+            this.comboBoxValues.Size = new System.Drawing.Size(400, 21);
+            this.comboBoxValues.TabIndex = 3;
+            // 
+            // labelAvKey
+            // 
+            this.labelAvKey.AutoSize = true;
+            this.labelAvKey.Location = new System.Drawing.Point(12, 68);
+            this.labelAvKey.Name = "labelAvKey";
+            this.labelAvKey.Size = new System.Drawing.Size(146, 13);
+            this.labelAvKey.TabIndex = 2;
+            this.labelAvKey.Text = "Aus Liste der verfügbaren Keys wählen:";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(183, 111);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 4;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(424, 146);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.comboBoxValues);
+            this.Controls.Add(this.labelAvKey);
+            this.Controls.Add(this.labelCodKey);
+            this.Controls.Add(this.textBoxCodKey);
+            this.Name = "Form1";
+            this.Text = "CoD4 Key Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cod4x_fixer
+{
+    public partial class Form1 : Form
+    {
+        private string userSid;
+        private string registryPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadUserSid();
+            LoadRegistryValue();
+            LoadComboBox();
+        }
+
+        private void LoadUserSid()
+        {
+            WindowsIdentity currentUser = WindowsIdentity.GetCurrent();
+            userSid = currentUser.User?.Value;
+            registryPath = $"HKEY_USERS\\{userSid}\\SOFTWARE\\Activision\\Call of Duty 4";
+        }
+
+        private void LoadRegistryValue()
+        {
+            string codKey = (string)Registry.GetValue(registryPath, "codkey", null);
+            textBoxCodKey.Text = codKey;
+        }
+
+        private void LoadComboBox()
+        {
+            string jsonFilePath = "keys.json";
+            var jsonData = File.ReadAllText(jsonFilePath);
+            var values = JsonConvert.DeserializeObject<string[]>(jsonData);
+            comboBoxValues.Items.AddRange(values);
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            string selectedValue = comboBoxValues.SelectedItem?.ToString();
+            if (selectedValue != null && selectedValue.Length == 20)
+            {
+                Registry.SetValue(registryPath, "codkey", selectedValue, RegistryValueKind.String);
+                MessageBox.Show("Registry-Eintrag erfolgreich aktualisiert!", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadRegistryValue();
+            }
+            else
+            {
+                MessageBox.Show("Bitte einen gültigen Wert auswählen.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
