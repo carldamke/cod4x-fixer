15979aa8a5c2537fd3ac691365fa28c1082b9fa0
Carl Damke <carl.simon.damke@gmail.com>
2024-07-18 23:26:05 +0200

    Saving windows username as key assignment information

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,12 @@
-﻿using Microsoft.Win32;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 
 namespace cod4x_fixer
 {
@@ -18,6 +14,7 @@ namespace cod4x_fixer
     {
         private string userSid;
         private string registryPath;
+        private List<KeyData> keyDataList;
 
         public Form1()
         {
@@ -25,6 +22,8 @@ namespace cod4x_fixer
             LoadUserSid();
             LoadRegistryValue();
             LoadComboBox();
+
+            Icon = new Icon("./cod4.ico");
         }
 
         private void LoadUserSid()
@@ -44,23 +43,40 @@ namespace cod4x_fixer
         {
             string jsonFilePath = "keys.json";
             var jsonData = File.ReadAllText(jsonFilePath);
-            var values = JsonConvert.DeserializeObject<string[]>(jsonData);
-            comboBoxValues.Items.AddRange(values);
+            keyDataList = JsonConvert.DeserializeObject<List<KeyData>>(jsonData);
+
+            comboBoxValues.Items.Clear();
+            comboBoxValues.Items.AddRange(keyDataList.ToArray());
         }
 
         private void buttonUpdate_Click(object sender, EventArgs e)
         {
-            string selectedValue = comboBoxValues.SelectedItem?.ToString();
-            if (selectedValue != null && selectedValue.Length == 20)
+            if (comboBoxValues.SelectedItem is KeyData selectedKeyData && selectedKeyData.Key.Length == 20)
             {
-                Registry.SetValue(registryPath, "codkey", selectedValue, RegistryValueKind.String);
-                MessageBox.Show("Registry-Eintrag erfolgreich aktualisiert!", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Registry.SetValue(registryPath, "codkey", selectedKeyData.Key, RegistryValueKind.String);
+                UpdateJsonFile(selectedKeyData.Key);
+                MessageBox.Show("Registry-Eintrag wurde aktualisiert.");
                 LoadRegistryValue();
+                LoadComboBox();
             }
             else
             {
-                MessageBox.Show("Bitte einen gültigen Wert auswählen.", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Bitte wählen Sie einen gültigen Key aus.");
             }
+        }
+
+        private void UpdateJsonFile(string selectedKey)
+        {
+            string jsonFilePath = "keys.json";
+            foreach (var keyData in keyDataList)
+            {
+                if (keyData.Key == selectedKey)
+                {
+                    keyData.User = Environment.UserName;
+                    break;
+                }
+            }
+            File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(keyDataList, Formatting.Indented));
         }
     }
 }
---
+++ KeyData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cod4x_fixer
+{
+    public class KeyData
+    {
+        public string Key { get; set; }
+        public string User { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Key} - {User}";
+        }
+    }
+
+}
